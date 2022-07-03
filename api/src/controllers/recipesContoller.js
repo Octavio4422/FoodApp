1d94a33a2ec342b67ae88c8bf4721b83ac634928
Octavio Paz <octaviopaz02@gmail.com>
2022-07-03 03:57:08 -0300

    ¿back updates

--- api/src/controllers/recipesContoller.js
+++ api/src/controllers/recipesContoller.js
@@ -1,19 +1,42 @@
 require("dotenv").config();
-const { Recipe, Diet } = require("../db.js");
 const axios = require("axios");
+const { Recipe, Diet } = require("../db.js");
 const { Op } = require("sequelize");
+const { apiParser } = require("../utils/functions/apiParser.js");
+const { response } = require("express");
 const { API_KEY } = process.env;
 
 function getRecipes(req, res, next) {
+  const { name } = req.query;
+  let apiResponse;
 
+  if (!name) {
+    axios
+      .get(
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`
+      )
+      .then((response) => {
+        apiResponse = response.data.results.map((r) => apiParser(r));
+        return Recipe.findAll({ include: Diet });
+      })
+      .then((dbresponse) => {
+        return res.status(200).json([...dbresponse, ...apiResponse]);
+      })
+      .catch((e) => {
+        return next(e);
+      });
+  }
+
+  if (name) {
+  }
 }
 
 function getRecipeId(req, res, next) {
-
+  const { id } = req.params;
 }
 
 function postRecipe(req, res, next) {
-
+  const { name, resume, score, steps } = req.body;
 }
 
 module.exports = { getRecipes, getRecipeId, postRecipe };
--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,28 +1,35 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("recipe", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
+  sequelize.define(
+    "recipe",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+      },
+      summary: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      score: {
+        type: DataTypes.INTEGER,
+      },
+      steps: {
+        type: DataTypes.STRING,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    resume:{
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    score:{
-      type: DataTypes.INTEGER,
-    },
-    steps:{
-      type: DataTypes.STRING,
+    {
+      timestamps: false,
     }
-  },{
-    timestamps: false
-  });
+  );
 };
